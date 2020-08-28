c708733b98c59e1d3a79d2620b4871d9c761294c
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 14:32:18 -0400

    Card component layout

---
+++ src/components/KanBanCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class KanBanCard extends React.Component {
+    render() {
+        return (
+            // OUTER CARD
+            <main className="card">
+                {/* CARD TITLE */}
+                <section className="card-title">
+
+                </section>
+                {/* CARD DESCRIPTION */}
+                <section className="card-descr">
+                    
+                </section>
+                
+                
+            </main>
+        )
+    }
+}
+
+export default KanBanCard;
