89d0ffbc589f19c380298715a7a2c4a4d85c4915
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 16:03:12 -0400

    Removed target value changes

--- src/components/ShowDescr.js
+++ src/components/ShowDescr.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 
 export default function ShowDescr() {
-    const openDescr = () => {
+    const openDescr = (e) => {
+        let clicked = e.target.id;
+        console.log(clicked);
         document.querySelector(".card-descr").style.visibility="visible";
         document.querySelector(".card").style.height="200px";
     }
