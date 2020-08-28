c3ea597f84f84f65d1a71945a75cd137eb3fc569
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 14:27:31 -0400

    Column component created

---
+++ src/components/KanBanColumn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class KanBanColumn extends React.Component {
+    render() {
+        return (
+            <main className="column">
+
+            </main>
+        )
+    }
+}
+
+export default KanBanColumn;
