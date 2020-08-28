c380723f6a975ac6aae3074d6978da0459805a24
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 16:51:19 -0400

    Trying to setState when btn clicked

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import KanBanCard from './KanBanCard';
 
-
-export default function AddCard() {
-    return (
-        <button className="card-add">
+class AddCard extends React.Component { 
+    constructor(props) {
+        super(props);
+        // State for add card btn
+        this.state = {
+            addACardClicked: false,
+        }
+    }
+    // When clicked set state to true
+    addACard() {
+        // console.log(this.addACardClicked)
+        this.setState(
+            { addACardClicked: true }
+            )
+       
+    }
+    render () {
+        const addACardClicked = this.state.addACardClicked;
+        if (addACardClicked) {
+            return <KanBanCard />
+            } else {
+            // Do nothing
+            }
+        return (
+             <button onClick={this.addACard} className="card-add">
             +
         </button>
     )
+    }
 }
+export default AddCard;
