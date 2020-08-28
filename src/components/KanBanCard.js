bc911e7627e676c7726443f65d635e2d2bc943ef
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 16:02:05 -0400

    Title and description now input elements to take values for editing

--- src/components/KanBanCard.js
+++ src/components/KanBanCard.js
@@ -2,19 +2,36 @@ import React from 'react';
 import ShowDescr from './ShowDescr';
 
 class KanBanCard extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            cardTitle: "Task Title",
+            cardDescr: "Task description",
+        }
+    }
+    onCardTitleChange = (e) => {
+        this.setState({
+            cardTitle: e.target.value,
+        })
+    }
+    onCardDescrChange = (e) => {
+        this.setState({
+            cardDescr: e.target.value,
+        })
+    }
     render() {
         return (
             // OUTER CARD
             <main className="card">
                 {/* CARD TITLE */}
                 <section className="card-title">
-                    <h1> TEST card</h1>
+                    <input type="text" value={this.state.cardTitle} onChange={this.onCardTitleChange} />
                     <ShowDescr />
                 </section>
                 {/* CARD DESCRIPTION */}
                 <section id="card-descr" className="card-descr">
-                    <p> Description here</p>
+                    {/* <p>{this.state.cardDescr}</p> */}
+                    <input type="text" value={this.state.cardDesc} onChange={this.onCardDescrChange} />
                 </section>
                 
                 
