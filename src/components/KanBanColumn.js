8d60799cd97a6916e90cda25de59d11930a80eb4
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 14:36:49 -0400

    Import KanBanCard

--- src/components/KanBanColumn.js
+++ src/components/KanBanColumn.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import KanBanCard from './KanBanCard';
 
 class KanBanColumn extends React.Component {
+    
     render() {
         return (
             // OUTER COLUMN
             <main className="column">
                 {/* COLUMN TITLE */}
                 <section className="column-title">
-
+                    <h1></h1>
                 </section>
                 {/* COLUMN CARDS */}
                 <section className="column-card">
