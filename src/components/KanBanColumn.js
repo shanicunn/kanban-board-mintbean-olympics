b05f5a8cd6a0eeb16c8967c357583bd69d2c6c46
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 14:29:44 -0400

    Column layout - title, card

--- src/components/KanBanColumn.js
+++ src/components/KanBanColumn.js
@@ -3,8 +3,18 @@ import React from 'react';
 class KanBanColumn extends React.Component {
     render() {
         return (
+            // OUTER COLUMN
             <main className="column">
+                {/* COLUMN TITLE */}
+                <section className="column-title">
 
+                </section>
+                {/* COLUMN CARDS */}
+                <section className="column-card">
+                    <KanBanCard />
+                </section>
+                
+                
             </main>
         )
     }
