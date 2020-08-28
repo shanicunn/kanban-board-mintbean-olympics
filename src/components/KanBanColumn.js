f94e9453a800f8adbf5d3fe819d5e1c51c03afe7
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 14:50:30 -0400

    Test text for visual

--- src/components/KanBanColumn.js
+++ src/components/KanBanColumn.js
@@ -2,14 +2,14 @@ import React from 'react';
 import KanBanCard from './KanBanCard';
 
 class KanBanColumn extends React.Component {
-    
+
     render() {
         return (
             // OUTER COLUMN
             <main className="column">
                 {/* COLUMN TITLE */}
                 <section className="column-title">
-                    <h1></h1>
+                    <h1>TEST COLUMN</h1>
                 </section>
                 {/* COLUMN CARDS */}
                 <section className="column-card">
