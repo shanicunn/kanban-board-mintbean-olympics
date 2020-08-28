68e46ebfe03706f3f486e0448c2e0cb2293bc49e
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 15:05:29 -0400

    Button layout completed

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export function AddCard() {
+export default function AddCard() {
     return (
         <button className="card-add">
-
+            +
         </button>
     )
 }
