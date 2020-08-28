dd9738f4233e92ece571b92551e2f2fd037cb703
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 14:50:11 -0400

    Test text for visual

--- src/components/KanBanCard.js
+++ src/components/KanBanCard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 class KanBanCard extends React.Component {
-    
+
     render() {
         return (
             // OUTER CARD
             <main className="card">
                 {/* CARD TITLE */}
                 <section className="card-title">
-                    <h1> </h1>
+                    <h1> TEST card</h1>
                 </section>
                 {/* CARD DESCRIPTION */}
                 <section className="card-descr">
-                    <p> </p>
+                    <p> Description here</p>
                 </section>
                 
                 
