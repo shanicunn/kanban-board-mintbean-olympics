d549fb133f7f365b030f4725f554ac7254c5f52a
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 17:13:35 -0400

    setState still won't change to false onClick

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,34 +1,38 @@
 import React from 'react';
-import KanBanCard from './KanBanCard';
+// import KanBanCard from './KanBanCard';
 
 class AddCard extends React.Component { 
-    constructor(props) {
-        super(props);
-        // State for add card btn
+    constructor() {
+        super();
+        // Default state for the add card btn
         this.state = {
             addACardClicked: false,
         }
     }
     // When clicked set state to true
     addACard() {
-        // console.log(this.addACardClicked)
-        this.setState(
+        //  setState was undefined without arrow function in button onClick
+            this.setState(
             { addACardClicked: true }
             )
-       
+            console.log(this.addACardClicked);
     }
     render () {
-        const addACardClicked = this.state.addACardClicked;
-        if (addACardClicked) {
-            return <KanBanCard />
-            } else {
-            // Do nothing
-            }
+        // const addACardClicked = this.state.addACardClicked;
+        
+        // if (addACardClicked) {
+        //     console.log(addACardClicked);
+        //     return <KanBanCard />
+        //     } else {
+        //     // Do nothing
+        //     console.log(addACardClicked);
+        //     }
         return (
-             <button onClick={this.addACard} className="card-add">
-            +
-        </button>
-    )
+            
+            <button onClick={() => this.addACard} className="card-add">
+                +
+            </button>
+        )
     }
 }
 export default AddCard;
