ff9e9d02e47922a68c338544041157f2ff6ceb3c
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-29 16:42:35 -0400

    Footer text added

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <main className="footer">
+            <h1 className="footer-h1">Kanban Board created by Shani Cunningham, Aidarous Albadawi, Jessica Sanchez</h1>
+            <h2 className="footer-h2">for <a href="https://sites.google.com/mintbean.io/javascriptbootcampolympics/home">Mintbean's</a> Javascript Hackathon Challenge </h2>
+        </main>
+    )
+} 
