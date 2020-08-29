a6447ca3a4ba520758b305a0d20fe21f9820d7da
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-28 20:13:59 -0400

    Trying to add id numbers to individual cards

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import KanBanCard from './KanBanCard';
 
+// FUNCTION TO ADD A CARD
 class AddCard extends React.Component { 
     constructor() {
         super();
@@ -11,41 +12,50 @@ class AddCard extends React.Component {
     }
     // When clicked set clicked state to true
     //  setState was undefined without arrow function in button onClick
-    setTrue = () => {
+    setTrue() {
         this.setState(
                 { clicked: true }
             )
             }
     // When clicked set clicked state to fale
-    setFalse = () => {
+    setFalse() {
         this.setState(
             { clicked: false }
             )
     }
     render () {
+        
+        
         // Variable for conditional statement
         const addACardClicked = this.state.clicked;
         // When clicked a card will render along with the add card btn
         // Only 1 task generates
+        // NO CARD DISPLAYED ON DEFAULT
         if (addACardClicked === false) {
             console.log("Button currently set to false");
             return ( 
                 <span>
-                <button onClick={this.setTrue} className="card-add">
-                    +
-                </button>
-                <KanBanCard />
-               
+                    <button onClick={() => this.setTrue()} className="card-add">
+                        +
+                    </button>
+                {/* <KanBanCard /> */}
             </span>
             )
             } 
+            // DISPLAYS CARD ON CLICK
+            let count = this.state.id;
         if (addACardClicked === true) {
             console.log("Button currently set to true");
+            count ++;
+            console.log("ID number: ", this.count);
             return (
                 <span>
-                    <button onClick={this.setFalse} className="card-add">
+                    <button onClick={() => this.setFalse()} className="card-add">
                         +
                     </button>
+                    
+                    <KanBanCard id={this.count}/>
+                    
                 </span>
             )
         }
