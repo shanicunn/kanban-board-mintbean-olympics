104a139e03b0c7c641f9a4e1becbc1b4931ba4b9
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-29 17:14:27 -0400

    Added more cmmts

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Footer from './components/Footer';
 
 function App() {
   return (
+    // WHOLE PAGE
     <div className="App">
+    {/* PAGE TITLE */}
       <section>
         <h1 className="board-title">KANBAN BOARD</h1>
       </section>
+      {/* KANBAN BOARD */}
       <section>
         <KanBanBoard />
       </section>
+      {/* FOOTER */}
       <footer className="footer">
         <Footer />
       </footer>
-        
-      
     </div>
   );
 }
