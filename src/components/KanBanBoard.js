90a96f84aa4aa278cb35be7a3e31c509921b668f
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-29 22:29:25 -0400

    Pushing to create new branch

--- src/components/KanBanBoard.js
+++ src/components/KanBanBoard.js
@@ -1,7 +1,48 @@
 import React from 'react';
 import KanBanColumn from './KanBanColumn';
 
+// https://www.youtube.com/watch?v=3OjNAp5GPYY
+// Column index directionals
+// const moveLeft = -1;
+// const moveRight = 1;
+
 class KanBanBoard extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     // Setting states for default columns and cards
+    //     this.state = {
+    //         columns: [
+    //             {
+    //                 columnTitle: "Todo",
+    //                 cards: [
+    //                     {
+    //                         cardTitle: "Title Task",
+    //                     },
+    //                 ]
+    //             },
+    //             {
+    //                 columnTitle: "In Progress",
+    //                 cards: [
+    //                     {
+    //                         cardTitle: "Title Task",
+    //                     },
+    //                 ]
+    //             },
+    //             {
+    //                 columnTitle: "Done",
+    //                 cards: [
+    //                     {
+    //                         cardTitle: "Title Task",
+    //                     },
+    //                 ]
+    //             },
+    //         ]
+    //     }
+    // }    
+    // addCard = columnIndex => {
+    //     const name = window.prompt
+    // }
+
     render() {
         return (
             // OUTER BOARD
